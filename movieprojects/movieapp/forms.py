210fc7d3e49b17a7fc9e58ddcd4f29bca385bf0e
vishnukk302 <vishnukk302@gmail.com>
2023-09-10 11:58:57 +0530

    added

---
+++ movieprojects/movieapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import movies
+
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model = movies
+        fields = ['name', 'desc', 'year', 'img']
---
+++ movieprojects/movieapp/views.py
@@ -0,0 +1,47 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .models import movies
+from .forms import MovieForm
+
+
+# Create your views here.
+def index(request):
+    movie = movies.objects.all()
+    context = {
+        'movie_list': movie
+    }
+    return render(request, 'index.html', context)
+
+
+def detail(request, movie_id):
+    movie = movies.objects.get(id=movie_id)
+    return render(request, "detail.html", {'movie': movie})
+
+
+def add_movie(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        desc = request.POST.get('desc')
+        year = request.POST.get('year')
+        img = request.FILES['img']
+        movie = movies(name=name, desc=desc, year=year, img=img)
+        movie.save()
+    return render(request, 'add.html')
+
+
+def update(request, id):
+    movie = movies.objects.get(id=id)
+    form = MovieForm(request.POST or None, request.FILES, instance=movie)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, 'edit.html', {'form': form, 'movie': movie})
+
+
+def delete(request,id):
+    if request.method == 'POST':
+        movie = movies.objects.get(id=id)
+        movie.delete()
+        return redirect('/')
+    return render(request,'delete.html')
